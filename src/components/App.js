eb2be4acf4fcbea6ecc757284bc5e2cc7df58130
Rinbo <robin.b@outlook.com>
2019-07-31 20:32:55 +0200

    Make note to self regarding 1 hour lost trying to deploy to gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,12 @@ import Game from "./game/Game";
 import { Route, Router, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
+/** 
+ * Note to future self:
+ * Prepending Router path with ${process.env.PUBLIC_URL} is imperative for client rounter to work with gh-pages.
+ * Furhermore, if using relative path (ie: https://rinbo.github.io/<relative-path>) change segmentCount to 1 in 404.html
+ */ 
+
 const routes = () => (
   <Switch>
     <Route path={`${process.env.PUBLIC_URL}/`} exact render={props => <LandingPage props={props} />} />
