0c3dc812a2f4f10c66b31769630114469f761687
Rinbo <robin.b@outlook.com>
2019-08-02 18:21:54 +0200

    Draw out the rim and board lines

--- src/components/game/Canvas.js
+++ src/components/game/Canvas.js
@@ -2,7 +2,9 @@ import React, { useState, useRef, useEffect } from "react";
 
 const CANVAS_WIDTH = 300;
 const CANVAS_HEIGHT = 500;
-const STRIKER_RADIUS = 30;
+const STRIKER_RADIUS = 15;
+const RIM_WIDTH = 10;
+const BOARD_LINE_WIDTH = 2;
 
 const Canvas = () => {
   const [onStriker, setOnStriker] = useState(false);
@@ -23,16 +25,90 @@ const Canvas = () => {
   });
 
   const initBoard = () => {
+    // The board
     ctx.current.beginPath();
+    ctx.current.lineWidth = RIM_WIDTH;
+    ctx.current.strokeStyle = "black";
+
+    // Left
+    ctx.current.moveTo(0, 0);
+    ctx.current.lineTo(0, CANVAS_HEIGHT);
+
+    // Top Left
+    ctx.current.moveTo(0, 0);
+    ctx.current.lineTo(CANVAS_WIDTH * 0.3, 0);
+
+    // Top Right
+    ctx.current.moveTo(CANVAS_WIDTH * 0.7, 0);
+    ctx.current.lineTo(CANVAS_WIDTH, 0);
+
+    // Right
+    ctx.current.moveTo(CANVAS_WIDTH, 0);
+    ctx.current.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    // Bottom Left
+    ctx.current.moveTo(0, CANVAS_HEIGHT);
+    ctx.current.lineTo(CANVAS_WIDTH * 0.3, CANVAS_HEIGHT);
+
+    // Bottom Right
+    ctx.current.moveTo(CANVAS_WIDTH * 0.7, CANVAS_HEIGHT);
+    ctx.current.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    ctx.current.stroke();
+    ctx.current.closePath();
+
+    // Some lines
+    ctx.current.beginPath();
+    ctx.current.lineWidth = BOARD_LINE_WIDTH;
+    ctx.current.strokeStyle = "red";
+    ctx.current.moveTo(RIM_WIDTH / 2, CANVAS_HEIGHT / 2);
+    ctx.current.lineTo(CANVAS_WIDTH - RIM_WIDTH / 2, CANVAS_HEIGHT / 2);
+    ctx.current.stroke();
+    ctx.current.closePath();
+
+    // Top Goal Arc
+    ctx.current.beginPath();
+
+    ctx.current.strokeStyle = "blue";
     ctx.current.arc(
-      striker1.centerX,
-      striker1.centerY,
-      striker1.radius,
+      CANVAS_WIDTH / 2,
+      RIM_WIDTH / 2,
+      (CANVAS_WIDTH - RIM_WIDTH) / 2,
+      0,
+      Math.PI
+    );
+    ctx.current.stroke();
+    ctx.current.closePath();
+
+    // Bottom Goal Arc
+    ctx.current.beginPath();
+
+    ctx.current.strokeStyle = "blue";
+    ctx.current.arc(
+      CANVAS_WIDTH / 2,
+      CANVAS_HEIGHT- RIM_WIDTH / 2,
+      (CANVAS_WIDTH - RIM_WIDTH) / 2,
       0,
       2 * Math.PI
     );
     ctx.current.stroke();
-    ctx.current.fill();
+    ctx.current.closePath();
+
+     // The striker
+     ctx.current.beginPath();
+     ctx.current.lineWidth = RIM_WIDTH;
+     ctx.current.strokeStyle = "black";
+     ctx.current.fillStyle = "grey";
+     ctx.current.arc(
+       striker1.centerX,
+       striker1.centerY,
+       striker1.radius,
+       0,
+       2 * Math.PI
+     );
+     ctx.current.stroke();
+     ctx.current.fill();
+     ctx.current.closePath();
   };
 
   const getMousePos = e => {
@@ -46,8 +122,6 @@ const Canvas = () => {
 
   const startPosition = e => {
     const pos = getMousePos(e);
-    console.log("PosX: ", pos.x);
-    console.log("PosY: ", pos.y);
     if (!checkIfInCircle(pos)) return;
     setOnStriker(true);
   };
@@ -65,15 +139,16 @@ const Canvas = () => {
   };
 
   const withinXBounds = x => {
-    if (x > CANVAS_WIDTH - STRIKER_RADIUS) return CANVAS_WIDTH - STRIKER_RADIUS;
-    if (x < STRIKER_RADIUS) return STRIKER_RADIUS;
+    if (x > CANVAS_WIDTH - STRIKER_RADIUS - RIM_WIDTH)
+      return CANVAS_WIDTH - STRIKER_RADIUS - RIM_WIDTH;
+    if (x < STRIKER_RADIUS + RIM_WIDTH) return STRIKER_RADIUS + RIM_WIDTH;
     return x;
   };
 
   const withinYBounds = y => {
-    if (y > CANVAS_HEIGHT/2 - STRIKER_RADIUS)
-      return CANVAS_HEIGHT/2 - STRIKER_RADIUS;
-    if (y < STRIKER_RADIUS) return STRIKER_RADIUS;
+    if (y > (CANVAS_HEIGHT / 2) - RIM_WIDTH/2 - STRIKER_RADIUS)
+      return (CANVAS_HEIGHT / 2) - RIM_WIDTH/2 - STRIKER_RADIUS;
+    if (y < STRIKER_RADIUS + RIM_WIDTH) return STRIKER_RADIUS + RIM_WIDTH;
     return y;
   };
 
