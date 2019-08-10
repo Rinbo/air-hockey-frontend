941e9efacb7a05701907fba288d7a75500f48927
Rinbo <robin.b@outlook.com>
2019-08-10 21:37:56 +0200

    Add eventReducer

---
+++ src/components/reducers/eventReducer.js
@@ -0,0 +1,25 @@
+const eventReducer = (state, { event, payload }) => {
+  switch (event) {
+    case "phx_reply":
+      return {
+        ...state,
+        message: payload.response.message || "Connection established"
+      };
+    case "ok":
+      return { ...state, message: payload.response.message };
+    case "game_started":
+      return { ...state, message: "Game successfully initialized" };
+    case "player_added":
+      return { ...state, message: payload.message };
+    case "subscribers":
+      return { ...state, subscribers: payload };
+    case "error":
+      console.log(event);
+      console.log(payload);
+      return { ...state, message: payload.message };
+    default:
+      return state;
+  }
+};
+
+export default eventReducer;
