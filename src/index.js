0972effeb98b10695ad058383e8036a00507faf4
Rinbo <robin.b@outlook.com>
2019-09-15 20:47:05 +0200

    Move urls out of .env to its own static js file in order to work with netlify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import {
+  PRODUCTION_BACKEND_URL,
+  LOCALHOST,
+  PRODUCTION_FRONTEND_URL,
+  NETLIFY_FRONTEND_URL
+} from "./urls";
 import { UserProvider } from "./components/contexts/UserContext";
 import { SocketProvider } from "./components/contexts/SocketContext";
 import "./css/app.css";
@@ -11,10 +17,13 @@ const AppWrapper = () => {
   let backendHost;
   const hostname = window && window.location && window.location.hostname;
 
-  if (hostname === process.env.REACT_APP_PRODUCTION_FRONTEND_URL) {
-    backendHost = process.env.REACT_APP_PRODUCTION_BACKEND_URL;
+  if (
+    hostname === PRODUCTION_FRONTEND_URL ||
+    hostname === NETLIFY_FRONTEND_URL
+  ) {
+    backendHost = PRODUCTION_BACKEND_URL;
   } else {
-    backendHost = process.env.REACT_APP_LOCALHOST;
+    backendHost = LOCALHOST;
   }
 
   return (
---
+++ src/urls.js
@@ -0,0 +1,5 @@
+export const PRODUCTION_BACKEND_URL =
+  "wss://airhockey-backend.borjessons.nu/socket";
+export const LOCALHOST = "ws://localhost:4000/socket";
+export const PRODUCTION_FRONTEND_URL = "airhockey.borjessons.nu";
+export const NETLIFY_FRONTEND_URL = "airhockey.netlify.com";
