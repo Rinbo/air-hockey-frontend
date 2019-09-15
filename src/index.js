740c2aa7fa3cbc949b45608eef25f676d77b78d6
Rinbo <robin.b@outlook.com>
2019-09-15 13:43:24 +0200

    Update production url in package.json

--- src/index.js
+++ src/index.js
@@ -7,14 +7,10 @@ import "./css/app.css";
 import "./css/navigation.css";
 import "./css/modal.css";
 
-// ws://ec2-13-48-147-103.eu-north-1.compute.amazonaws.com:4000/socket
-// ws://localhost:4000/socket
-
 const AppWrapper = () => {
-
   let backendHost;
   const hostname = window && window.location && window.location.hostname;
-  
+
   if (hostname === process.env.REACT_APP_PRODUCTION_FRONTEND_URL) {
     backendHost = process.env.REACT_APP_PRODUCTION_BACKEND_URL;
   } else {
